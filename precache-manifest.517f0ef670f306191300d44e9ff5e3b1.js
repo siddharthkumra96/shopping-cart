1f25f2f8a80889ddfbc3e55094fc21f690d85f87
siddharthkumra@vinsol.com <siddharthkumra@vinsol.com>
2019-11-20 14:58:27 +0530

    Updates

---
+++ precache-manifest.517f0ef670f306191300d44e9ff5e3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842a9189306b953e334871c6746dec16",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "312dc271645154cead4d",
+    "url": "/shopping-cart/static/css/main.94b983d1.chunk.css"
+  },
+  {
+    "revision": "2a1fa1ecd9aa7760a88f",
+    "url": "/shopping-cart/static/js/2.3689200c.chunk.js"
+  },
+  {
+    "revision": "312dc271645154cead4d",
+    "url": "/shopping-cart/static/js/main.5beb4876.chunk.js"
+  },
+  {
+    "revision": "4a3f8d78162ea9a885b8",
+    "url": "/shopping-cart/static/js/runtime-main.d8b043e7.js"
+  }
+]);
